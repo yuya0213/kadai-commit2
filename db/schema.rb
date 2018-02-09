e9e96c6e031b40f373e0cc815001e982ac113f17
jiro.kirameki <yuya0213@outlook.jp>
2018-02-09 12:59:08 +0000

    コミットメッセージ

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180206121554) do
+ActiveRecord::Schema.define(version: 20180209104123) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
 end
