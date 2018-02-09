cb5cf56a5841290d2cb06d091bad9a3994f70dcd
jiro.kirameki <yuya0213@outlook.jp>
2018-02-09 10:55:28 +0000

    コミットメッセージ

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -53,6 +53,6 @@ class TasksController < ApplicationController
   end
   
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end 
