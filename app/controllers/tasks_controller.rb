45908b2423bf41d06ca1346c8785bc962362d70f
jiro.kirameki <yuya0213@outlook.jp>
2018-02-09 09:45:00 +0000

    コミットメッセージ

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
- def index
-   @tasks = Task.all
- end
+  def index
+    @tasks = Task.all
+  end
 
   def show
    @task = Task.find(params[:id])
@@ -27,8 +27,27 @@ class TasksController < ApplicationController
    @task = Task.find(params[:id])
   end
   
-   private
+  def update
+    @task = Task.find(params[:id])
 
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url # "https://techacademy-yuya0213.c9users.io/tasks"
+  end
+  
+  private
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content)
