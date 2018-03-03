0eeea0b20c62ff62f9f0654ab6b84f11625941c1
jiro.kirameki <yuya0213@outlook.jp>
2018-03-03 13:12:31 +0000

    tasklogin

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+  def index
+    @users = User.all.page(params[:page])
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htmlre
  root to: 'tasks#index'
+ 
+ get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
+
+ get 'signup', to: 'users#new'
  resources :tasks
 end
